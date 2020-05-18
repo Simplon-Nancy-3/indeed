b8f3442f722db054ecd85293996cc70369fee083
= <=>
2020-05-18 11:49:15 +0200

    major improvement on jobs extraction

--- scrap.py
+++ scrap.py
@@ -3,18 +3,26 @@ import requests
 import re, json
 import ast
 
-# req = requests.get('https://www.indeed.fr/emplois?q=data scientist&l&advn=1699844263147621&vjk=742278d478124f60')
+def extract_kv(str_):
+    kv = str_.split('\',')
+    for i in range(len(kv)):
+        k, v = kv[i].split(':') 
+        kv[i] = (k, v[1:-1])
+    return kv
+
+def dict_from_str(str_, dict_):
+    kv = extract_kv(str_)
+    dict_.update({kv[0][1]:{k:v for k, v in kv[1:]}})
+    return dict_
+
+# req = requests.get('https://www.indeed.fr/emplois?q=data+scientist&start=0')
 # with open("html/sample_0.html", 'w') as f:
 #     f.write(BeautifulSoup(req.text, features="html.parser").prettify())
 
-def dict_from_str(dict_str):
-    kv = dict_str.split(',')
-    for i in range(len(kv)):
-        k, v = kv[i].split(':')        
-        kv[i] = '\'{}\':{}'.format(k, v) 
-    return '{%s}'%(','.join(kv))
-
-
+res = {}
 with open("html/sample_0.html", 'r') as f:
-    with open('json/sample_0.json', 'w') as fw:
-        fw.write(json.dumps([json.loads(dict_from_str(hit).replace('\'', '"')) for hit in re.findall(r'jobmap\[[0-9]+\]= {([^}]*)}', f.read())]))
+    for hit in re.findall(r'jobmap\[[0-9]+\]= {([^}]*)}', f.read()):
+        dict_from_str(hit, res)
+
+with open('json/sample_0.json', 'w') as fw:
+    fw.write(json.dumps(res))
