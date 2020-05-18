d3c44fa8c01ef81bfe12f183f2307f7778c1343f
= <=>
2020-05-18 11:25:47 +0200

    get json array of jobs

--- scrap.py
+++ scrap.py
@@ -1,6 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
+import re, json
+import ast
 
-req = requests.get('https://www.indeed.fr/emplois?q=data scientist&l&advn=1699844263147621&vjk=742278d478124f60')
-with open("html/sample_0.html", 'w') as f:
-    f.write(BeautifulSoup(req.text, features="html.parser").prettify())
+# req = requests.get('https://www.indeed.fr/emplois?q=data scientist&l&advn=1699844263147621&vjk=742278d478124f60')
+# with open("html/sample_0.html", 'w') as f:
+#     f.write(BeautifulSoup(req.text, features="html.parser").prettify())
+
+def dict_from_str(dict_str):
+    kv = dict_str.split(',')
+    for i in range(len(kv)):
+        k, v = kv[i].split(':')        
+        kv[i] = '\'{}\':{}'.format(k, v) 
+    return '{%s}'%(','.join(kv))
+
+
+with open("html/sample_0.html", 'r') as f:
+    with open('json/sample_0.json', 'w') as fw:
+        fw.write(json.dumps([json.loads(dict_from_str(hit).replace('\'', '"')) for hit in re.findall(r'jobmap\[[0-9]+\]= {([^}]*)}', f.read())]))
