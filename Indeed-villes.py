af18d65c772ecc1702b632b3483402734e0f7c1e
Vin-100 <60468251+Vin-100@users.noreply.github.com>
2020-05-28 18:40:24 +0200

    Add files via upload

--- Indeed-villes.py
+++ Indeed-villes.py
@@ -3,6 +3,9 @@
 Created on Wed May 27 08:49:17 2020
 
 @author: Vincent
+
+Liste des algos de regression linéaire : https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html
+
 """
 
 
@@ -19,6 +22,10 @@ import html2text
 path = 'C:/Users/Utilisateur/Google Drive/Simplon/Formation/indeed.projet/indeed-pierre/'
 #df = pd.read_csv(path+'indeed.csv', keep_default_na=False)  ## Les nan sont convertis en null
 df = pd.read_csv(path+'jobs_it_process.csv')  ## conserver les nan pour intégration dans le scrapping
+
+df.columns
+df.rename(columns={'location': 'location_dirty','region': 'region_p'}, inplace=True)
+df_region = df.dropna(subset=['salary_mean'], axis=0)
 
 '''
 # Région Parisienne  = 75 - 77 - 78- 91 - 92 - 93 -94 - 95
@@ -57,15 +64,15 @@ df = df.dropna(subset=['dep'], axis=0)
 df.isna().sum()
 
 def size(n):
-    if n in [75,77,78,91,92,93,94,95]:
+    if n in ['75','77','78','91','92','93','94','95']:
         return 'Paris'
-    elif n in [69, 1, 42, 38, 71]:
+    elif n in ['69', '01', '42', '38', '71']:
         return 'Lyon'
-    elif n in [33, 17, 16, 24, 47, 40]:
+    elif n in ['33', '17', '16', '24', '47', '40']:
         return 'Bordeaux'
-    elif n in [44, 56, 35, 53, 49, 85]:
+    elif n in ['44', '56', '35', '53', '49', '85']:
         return 'Nantes'
-    elif n in [31, 32, 82, 81, 11, 9, 65]:
+    elif n in ['31', '32', '82', '81', '11', '09', '65']:
         return 'Toulouse'
     else:
         return 'Ailleurs'
@@ -82,6 +89,7 @@ from sklearn.datasets import make_regression
 df = pd.concat([df,pd.get_dummies(df['region'])], axis=1)
 
 def base(df, region):
+    from sklearn.utils.validation import column_or_1d
     df_region = df[['salary_mean', 'query_developpeur', 'query_business+intelligence',
        'query_data+scientist', 'query_data+analyst', 'contract_temps plein',
        'contract_cdd', 'contract_apprentissage', 'contract_indépendant',
@@ -96,11 +104,56 @@ def base(df, region):
        'contract_stage', 'contract_temps partiel', 'contract_intérim',
        'contract_commission', 'contract_contrat pro', 'contract_cdi']]
     y = df_region[['salary_mean']]
+    y = column_or_1d(y, warn=True)
     #print(y.shape)
     return(X, y)
 
 
+def estime(df, region):
+    from sklearn.utils.validation import column_or_1d
+    df_region = df[['salary_mean', 'query_developpeur', 'query_business+intelligence',
+       'query_data+scientist', 'query_data+analyst', 'contract_temps plein',
+       'contract_cdd', 'contract_apprentissage', 'contract_indépendant',
+       'contract_stage', 'contract_temps partiel', 'contract_intérim',
+       'contract_commission', 'contract_contrat pro', 'contract_cdi',region]]
+    df_region = df_region[df_region[region]==1]
+
+    X = df_region[['query_developpeur', 'query_business+intelligence',
+       'query_data+scientist', 'query_data+analyst', 'contract_temps plein',
+       'contract_cdd', 'contract_apprentissage', 'contract_indépendant',
+       'contract_stage', 'contract_temps partiel', 'contract_intérim',
+       'contract_commission', 'contract_contrat pro', 'contract_cdi']]
+    y = df_region[['salary_mean']]
+    y = column_or_1d(y, warn=True)
+    #print(y.shape)
+    return(X, y)
+
+lyon = pd.DataFrame(base(df,'Lyon')[1], columns=['Lyon'])
+lyon.to_excel(r''+path+'lyon.xlsx', sheet_name='indeed', index = False)
+
+paris = pd.DataFrame(base(df,'Paris')[1], columns=['Lyon'])
+paris.to_excel(r''+path+'Paris.xlsx', sheet_name='indeed', index = False)
+
+from matplotlib import pyplot
+
+pyplot.plot(lyon, linestyle = 'none', marker = 'o', c = 'lime')
+pyplot.xlim(0, lyon.shape[0])
+pyplot.ylim(0, 110000)
+pyplot.title('Salaire sur Lyon')
+pyplot.show()
+pyplot.plot(paris, linestyle = 'none', marker = 'o', c = 'lime')
+pyplot.xlim(0, paris.shape[0])
+pyplot.ylim(0, 180000)
+pyplot.title('Salaire sur Paris')
+pyplot.show()
+
+
+
+,
+  markersize = 10)
+
 def full(df):
+    from sklearn.utils.validation import column_or_1d
     df_full = df[['salary_mean', 'query_developpeur', 'query_business+intelligence',
        'query_data+scientist', 'query_data+analyst', 'contract_temps plein',
        'contract_cdd', 'contract_apprentissage', 'contract_indépendant',
@@ -116,6 +169,7 @@ def full(df):
        'contract_commission', 'contract_contrat pro', 'contract_cdi', 'Ailleurs', 
        'Bordeaux', 'Lyon', 'Nantes', 'Paris', 'Toulouse']]
     y = df_full[['salary_mean']]
+    y = column_or_1d(y, warn=True)
     #print(y.shape)
     return(X, y)
 
@@ -124,11 +178,14 @@ for i in regions :
     X, y = base(df, i)
     print(i, y.shape)
 '''
+# Souci avec base Lyon...
+df_lyn = base(df,'Lyon')[1]
+
 
 def reglin_salaire(df, region) :
     X, y = base(df, region)
     #X, y = full(df)
-    
+    y = y.values.tolist()
     from sklearn.model_selection import train_test_split
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=0)
 
@@ -159,6 +216,8 @@ def analyse(X_test, y_test, model):
 # Regression linéaire de base...
 # =============================================================================
 
+print()
+print('LinearRegression')
 for i in regions :
     X, y = base(df, i)
     print(i,'=>')
@@ -185,31 +244,42 @@ clf.score(X_test, y_test)
 # GradientBoostingRegressor
 # =============================================================================
 
-import matplotlib.pyplot as plt
-import numpy as np
-from sklearn import datasets, ensemble
-from sklearn.inspection import permutation_importance
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
-X, y = base(df, 'Toulouse')
-#X, y = full(df)
+def gbr_salaire(X, y):
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from sklearn import datasets, ensemble
+    from sklearn.inspection import permutation_importance
+    from sklearn.metrics import mean_squared_error
+    from sklearn.model_selection import train_test_split
 
-y = y.values.tolist()
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.1, random_state=13)
-params = {'n_estimators': 500,
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=13)
+    params = {'n_estimators': 500,
           'max_depth': 4,
           'min_samples_split': 5,
           'learning_rate': 0.01,
           'loss': 'ls'}
-reg = ensemble.GradientBoostingRegressor(**params)
-reg.fit(X_train, y_train)
+    reg = ensemble.GradientBoostingRegressor(**params)
+    reg.fit(X_train, y_train)
 
-reg.predict(X_test)
-reg.score(X_test, y_test)
-analyse(X_test, y_test, reg)
+    reg.predict(X_test)
+    reg.score(X_test, y_test)
+    print(analyse(X_test, y_test, reg))
+
+print()
+print('GradientBoostingRegressor')
+print('full =>')
+X, y = full(df)
+gbr_salaire(X, y)
+for i in regions :
+    X, y = base(df, i)
+    print()
+    print(i,'=>')
+    gbr_salaire(X, y)
 
 
+
+
+'''
 mse = mean_squared_error(y_test, reg.predict(X_test))
 print("The mean squared error (MSE) on test set: {:.4f}".format(mse))
 
@@ -229,32 +299,47 @@ plt.xlabel('Boosting Iterations')
 plt.ylabel('Deviance')
 fig.tight_layout()
 plt.show()
-
+'''
 
 # =============================================================================
 # Regression avec KNN
 # =============================================================================
 
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn import neighbors
-from sklearn.model_selection import train_test_split
+def knn_salaire(X, y):
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from sklearn import neighbors
+    from sklearn.model_selection import train_test_split
 
-X, y = base(df, 'Lyon')
-#X, y = full(df)
-y = y.values.tolist()
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.40, random_state=3)
-# #############################################################################
-# Fit regression model
-n_neighbors = 5
+    X, y = base(df, 'Lyon')
+    #X, y = full(df)
+    y = y.values.tolist()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.40, random_state=3)
+    # #############################################################################
+    # Fit regression model
+    n_neighbors = 5
 
-for i, weights in enumerate(['uniform', 'distance']):
-    knn = neighbors.KNeighborsRegressor(n_neighbors, weights=weights)
-    y_ = knn.fit(X_train, y_train).predict(X_test)
+    for i, weights in enumerate(['uniform', 'distance']):
+        knn = neighbors.KNeighborsRegressor(n_neighbors, weights=weights)
+        y_ = knn.fit(X_train, y_train).predict(X_test)
 
-knn.predict(X_test)
-knn.score(X_test, y_test)
-analyse(X_test, y_test, knn)
+    knn.predict(X_test)
+    knn.score(X_test, y_test)
+    print(analyse(X_test, y_test, knn))
+
+
+print()
+print('KNeighborsRegressor')
+print('full =>')
+X, y = full(df)
+gbr_salaire(X, y)
+for i in regions :
+    X, y = base(df, i)
+    print()
+    print(i,'=>')
+    gbr_salaire(X, y)
+
+
 
 '''
     plt.subplot(2, 1, i + 1)
@@ -272,27 +357,31 @@ plt.show()
 
 
 # =============================================================================
-# Modèle OLS - Ordinary Least Squares
+# Modèle OLS - Ordinary Least Squares  --  Meilleur modèle ?
 # =============================================================================
 ## Without a constant
 def ols_salaire(X, y):
     import statsmodels.api as sm
     from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.40, random_state=13)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.4, random_state=3)
     model = sm.OLS(y_train, X_train).fit()
     predictions = model.predict(X_test) # make the predictions by the model
-    print(model.summary())
+    #print(model.summary())
+    predictions = model.predict(X)
+    
+    return(predictions)
 
 
 print()
 print('full =>')
 X, y = full(df)
-ols_salaire(X, y)
+sal=ols_salaire(X, y)
+
 for i in regions :
     X, y = base(df, i)
     print()
     print(i,'=>')
-    ols_salaire(X, y)
+    print(ols_salaire(X, y))
 
 
 
@@ -333,7 +422,7 @@ for i in regions :
 
 
 # =============================================================================
-# Modèle QuantileRegressor -- Pa strouvé comment ça fonctionne
+# Modèle QuantileRegressor -- Pas trouvé comment ça fonctionne
 # =============================================================================
 ## Nécessite : pip install skquantreg
 from skquantreg import QuantileRegressor
@@ -351,14 +440,14 @@ model.score(X_test, y_test)
 
 
 # =============================================================================
-# RecursiveLS - Moindres carrés récursifs
+# RecursiveLS - Moindres carrés récursifs -- nan en retour...
 # =============================================================================
 import statsmodels.api as sm
-X, y = base(df, 'Nantes')
-X, y = full(df)
+X, y = base(df, 'Lyon')
+#X, y = full(df)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.4, random_state=5)
 
-mod = sm.RecursiveLS(y_train, X_train)
+mod = sm.RecursiveLS(y, X)
 res = mod.fit()
 
 print(res.summary())
@@ -367,15 +456,27 @@ print(res.summary())
 # =============================================================================
 # Weighted Least Squares
 # =============================================================================
-import statsmodels.api as sm
-from statsmodels.sandbox.regression.predstd import wls_prediction_std
-X, y = base(df, 'Toulouse')
-X, y = full(df)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.4, random_state=5)
+def wls_salaire(X, y):
+    import statsmodels.api as sm
+    from statsmodels.sandbox.regression.predstd import wls_prediction_std
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.4, random_state=5)
+    mod_wls = sm.WLS(y_train, X_train)
+    res_wls = mod_wls.fit()
+    print(res_wls.summary())
 
-mod_wls = sm.WLS(y_train, X_train)
-res_wls = mod_wls.fit()
-print(res_wls.summary())
+print()
+print('Weighted Least Squares')
+print()
+print('full =>')
+X, y = full(df)
+glsar_salaire(X, y)
+for i in regions :
+    X, y = base(df, i)
+    print()
+    print(i,'=>',y.shape)
+    glsar_salaire(X, y)
+
+
 
 
 
